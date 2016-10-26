96cab52d7e539f1a412b69ee4f65cf12a2e46422
Moer <199525807@qq.com>
2016-10-26 15:27:41 +0800

    完善发现页面的筛选刷新

--- dist/js/find.js
+++ dist/js/find.js
@@ -1,8 +1,13 @@
-"use strict";
+'use strict';
 
 {
     (function () {
+        // 当前页
+
         var getFindData = function getFindData(data, callback) {
+            if (data.loadingModal) {
+                $.showIndicator();
+            }
             // 合并入筛选参数
             data = $.extend({}, data, {
                 sort: sort,
@@ -25,19 +30,67 @@
                 error: function error(e) {
                     console.log(e);
                     $.alert('刷新失败，请稍后再试！');
+                },
+                complete: function complete() {
+                    $.hideIndicator();
                 }
             });
         };
 
+        // 刷新数据
+
+
+        var reload = function reload() {
+            // 滚动条置顶
+            $infiniteScroll[0].scrollTop = 0;
+
+            // 回复loading的效果
+            $preloader.html('<div class="preloader"></div>');
+
+            // 当前页从1开始
+            currentPage = 1;
+
+            // 开启无限加载
+            loading = false;
+            getFindData({
+                loadingModal: true,
+                skip: 1, //当前页
+                limit: itemsPerLoad //每页条数
+            }, function (data) {
+                currentPage++;
+                // console.log('开始刷新', data);
+                $contanier.empty();
+                addItems(data);
+            });
+        };
+
+        // 加载完成
+
+
+        var _finish = function _finish() {
+            // 加载完毕，则注销无限加载事件，以防不必要的加载
+            // $.detachInfiniteScroll($('.infinite-scroll'));
+
+            // 在可能重复使用的页面，设置loading，暂时关闭加载
+            loading = true;
+
+            // 删除加载提示符
+            $preloader.text('已经到底了！');
+        };
+
+        // 顶部筛选
+
+
+        // infinite-scroll
         var addItems = function addItems(data) {
             // 生成新条目的HTML
             var html = '';
             for (var i = 0; i < data.length; i++) {
                 var d = data[i];
-                html += "\n            <a href=\"./movieDetails.html?movieId=" + d.id + "\" class=\"find-list\">\n                <div class=\"imgbox\">\n                    <img src=\"" + d.poster + "\" alt=\"\">\n                    <div class=\"status\">" + (d.updateStatus == 0 ? '已完结' : '更新中') + "</div>\n                </div>\n                <p class=\"name\">" + d.title + " " + d.updateSite + "</p>\n            </a>\n            ";
+                html += '\n            <a href="./movieDetails.html?movieId=' + d.id + '" class="find-list">\n                <div class="imgbox">\n                    <img src="' + d.poster + '" alt="">\n                    <div class="status">' + (d.updateStatus == 0 ? '已完结' : '更新中') + '</div>\n                </div>\n                <p class="name">' + d.title + ' ' + d.updateSite + '</p>\n            </a>\n            ';
             }
             // 添加新条目
-            $('.infinite-scroll-bottom .find-content').append(html);
+            $contanier.append(html);
         };
 
         //预先加载
@@ -45,11 +98,16 @@
 
         // picker
 
-        var sort = 1;
-        var area = null;
+        var $infiniteScroll = $('.infinite-scroll'); //滚动父容器
+        var $contanier = $('.find-content'); //列表容器
+        var $preloader = $infiniteScroll.find('.infinite-scroll-preloader'); //滚动加载loading效果
+        var maxItems = 60; // 最多可加载的条目
+        var itemsPerLoad = 9; // 每次加载添加多少条目
+        var loading = false; // 加载flag
+        var sort = 1; //排序 1.更新时间 2.人气排行
+        var area = null; //地区 直接传中文字符，'全部'传空
         var type = null;
-
-        var $selectSwitch = $('.select-switch');
+        var currentPage = 1;var $selectSwitch = $('.select-switch');
         $selectSwitch.on('click', function (e) {
             e.stopPropagation();
             var $this = $(this);
@@ -64,52 +122,39 @@
                 });
             }
         });
-
         $selectSwitch.eq(0).picker({
-            toolbarTemplate: "<header class=\"find-select\"></header>",
+            toolbarTemplate: '<header class="find-select"></header>',
             cols: [{
                 textAlign: 'center',
                 values: ['更新时间', '人气排行']
             }],
             onClose: function onClose(picker) {
-                console.log('当前值：', picker);
                 sort = picker.value[0] == '更新时间' ? 1 : 2;
+                reload();
             }
         });
         $selectSwitch.eq(1).picker({
-            toolbarTemplate: "<header class=\"find-select\"></header>",
+            toolbarTemplate: '<header class="find-select"></header>',
             cols: [{
                 textAlign: 'center',
                 values: ['全部', '内地', '韩国']
             }],
             onClose: function onClose(picker) {
                 area = picker.value[0] == '全部' ? null : picker.value[0];
+                reload();
             }
         });
         $selectSwitch.eq(2).picker({
-            toolbarTemplate: "<header class=\"find-select\"></header>",
+            toolbarTemplate: '<header class="find-select"></header>',
             cols: [{
                 textAlign: 'center',
                 values: ['全部', '电视剧', '电影']
             }],
             onClose: function onClose(picker) {
                 area = picker.value[0] == '全部' ? null : picker.value[0];
+                reload();
             }
-        });
-
-        // infinite-scroll
-        // 加载flag
-        var loading = false;
-        // 最多可加载的条目
-        var maxItems = 60;
-
-        // 每次加载添加多少条目
-        var itemsPerLoad = 9;
-
-        // 当前页
-        var currentPage = 1;
-
-        getFindData({
+        });getFindData({
             skip: 1, //当前页
             limit: itemsPerLoad //每页条数
         }, function (data) {
@@ -119,14 +164,7 @@
         });
 
         // 注册'infinite'事件处理函数
-        $(document).on('infinite', '.infinite-scroll-bottom', function () {
-            function _finish() {
-                // 加载完毕，则注销无限加载事件，以防不必要的加载
-                $.detachInfiniteScroll($('.infinite-scroll'));
-                // 删除加载提示符
-                $('.infinite-scroll-preloader').text('已经到底了！');
-            }
-
+        $infiniteScroll.on('infinite', function () {
             // 如果正在加载，则退出
             if (loading) return;
 
--- src/js/find.js
+++ src/js/find.js
@@ -1,10 +1,20 @@
 { // picker
 
-    let sort = 1
-    let area = null
+    const $infiniteScroll = $('.infinite-scroll') //滚动父容器
+    const $contanier = $('.find-content') //列表容器
+    const $preloader = $infiniteScroll.find('.infinite-scroll-preloader') //滚动加载loading效果
+    const maxItems = 60; // 最多可加载的条目
+    const itemsPerLoad = 9; // 每次加载添加多少条目
+    let loading = false; // 加载flag
+    let sort = 1 //排序 1.更新时间 2.人气排行
+    let area = null //地区 直接传中文字符，'全部'传空
     let type = null
+    let currentPage = 1; // 当前页
 
     function getFindData(data, callback) {
+        if (data.loadingModal) {
+            $.showIndicator()
+        }
         // 合并入筛选参数
         data = $.extend({}, data, {
             sort: sort,
@@ -27,10 +37,51 @@
             error: function (e) {
                 console.log(e);
                 $.alert('刷新失败，请稍后再试！')
+            },
+            complete:function(){
+                $.hideIndicator();
             }
         });
     }
 
+    // 刷新数据
+    function reload() {
+        // 滚动条置顶
+        $infiniteScroll[0].scrollTop = 0;
+
+        // 回复loading的效果
+        $preloader.html('<div class="preloader"></div>')
+
+        // 当前页从1开始
+        currentPage = 1;
+
+        // 开启无限加载
+        loading = false;
+        getFindData({
+            loadingModal: true,
+            skip: 1, //当前页
+            limit: itemsPerLoad //每页条数
+        }, (data) => {
+            currentPage++;
+            // console.log('开始刷新', data);
+            $contanier.empty()
+            addItems(data)
+        })
+    }
+
+    // 加载完成
+    function _finish() {
+        // 加载完毕，则注销无限加载事件，以防不必要的加载
+        // $.detachInfiniteScroll($('.infinite-scroll'));
+
+        // 在可能重复使用的页面，设置loading，暂时关闭加载
+        loading = true;
+
+        // 删除加载提示符
+        $preloader.text('已经到底了！');
+    }
+
+    // 顶部筛选
     let $selectSwitch = $('.select-switch')
     $selectSwitch.on('click', function (e) {
         e.stopPropagation();
@@ -46,8 +97,6 @@
             });
         }
     })
-
-
     $selectSwitch.eq(0).picker({
         toolbarTemplate: `<header class="find-select"></header>`,
         cols: [{
@@ -55,8 +104,8 @@
             values: ['更新时间', '人气排行']
         }],
         onClose: (picker) => {
-            console.log('当前值：', picker);
             sort = picker.value[0] == '更新时间' ? 1 : 2
+            reload()
         }
     });
     $selectSwitch.eq(1).picker({
@@ -67,6 +116,7 @@
         }],
         onClose: (picker) => {
             area = picker.value[0] == '全部' ? null : picker.value[0]
+            reload()
         }
     });
     $selectSwitch.eq(2).picker({
@@ -77,22 +127,12 @@
         }],
         onClose: (picker) => {
             area = picker.value[0] == '全部' ? null : picker.value[0]
+            reload()
         }
     });
 
 
     // infinite-scroll
-    // 加载flag
-    let loading = false;
-    // 最多可加载的条目
-    const maxItems = 60;
-
-    // 每次加载添加多少条目
-    const itemsPerLoad = 9;
-
-    // 当前页
-    let currentPage = 1;
-
     function addItems(data) {
         // 生成新条目的HTML
         let html = '';
@@ -109,7 +149,7 @@
             `
         }
         // 添加新条目
-        $('.infinite-scroll-bottom .find-content').append(html);
+        $contanier.append(html);
 
     }
 
@@ -125,14 +165,7 @@
 
 
     // 注册'infinite'事件处理函数
-    $(document).on('infinite', '.infinite-scroll-bottom', function () {
-        function _finish() {
-            // 加载完毕，则注销无限加载事件，以防不必要的加载
-            $.detachInfiniteScroll($('.infinite-scroll'));
-            // 删除加载提示符
-            $('.infinite-scroll-preloader').text('已经到底了！');
-        }
-
+    $infiniteScroll.on('infinite', function () {
         // 如果正在加载，则退出
         if (loading) return;
 
