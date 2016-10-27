42e4be6d5f11be2ffc096ba511b42bab5a2c8734
Moer <199525807@qq.com>
2016-10-27 17:26:17 +0800

    删除多余代码

--- dist/js/myMovie.js
+++ dist/js/myMovie.js
@@ -2,26 +2,6 @@
 
 {
     (function () {
-
-        // 创建电影列表
-        var createMyMovie = function createMyMovie(data) {
-            var tpl = '';
-            var length = 3;
-            for (var i = 0; i < length; i++) {
-                tpl += '\n            <a class="box" href="#">\n                <div class="imgbox">\n                    <img src="../images/index-banner.jpg" alt="">\n                </div>\n                <div class="info">\n                    <span class="Title">迷失东京</span>\n                    <p class="text">人生在世，孤苦无比，如果得知原有些人和你一般孤苦，是否心里会好受些？看《迷失东京》前，未曾料想会收获这样一种感觉。</p>\n                    <span class="text2">更新到第3集</span>\n                </div>\n                <span class="info2">\n                    <span>下单时间:<span class="day">9/12</span><span class="time">9:13</span></span>\n                    <span class="price">15.99</span>\n                </span>\n            </a>\n            ';
-            }
-            $('.myMovieList').append(tpl).removeClass('empty');
-        };
-        // createMyMovie();
-
-
-        // 清空所有电影列表
-
-
-        var emptyMyMovie = function emptyMyMovie() {
-            $('.myMovieList').empty().addClass('empty');
-        };
-
         var $contanier = $('.myMovieList');
 
         $.showIndicator();
@@ -57,7 +37,7 @@
                         $.alert('刷新失败，请稍后再试！');
                     },
                     complete: function complete() {
-                        $contanier.toggleClass('empty', Boolean($contanier.children().length));
+                        $contanier.toggleClass('empty', !!!$contanier.children().length);
                         $.hideIndicator();
                     }
                 });
--- src/js/myMovie.js
+++ src/js/myMovie.js
@@ -49,42 +49,11 @@
                     $.alert('刷新失败，请稍后再试！')
                 },
                 complete: () => {
-                    $contanier.toggleClass('empty',Boolean($contanier.children().length))
+                    $contanier.toggleClass('empty',!!!$contanier.children().length)
                     $.hideIndicator()
                 }
             });
         }
     })
 
-    // 创建电影列表
-    function createMyMovie(data) {
-        let tpl = ``
-        let length = 3;
-        for (let i = 0; i < length; i++) {
-            tpl += `
-            <a class="box" href="#">
-                <div class="imgbox">
-                    <img src="../images/index-banner.jpg" alt="">
-                </div>
-                <div class="info">
-                    <span class="Title">迷失东京</span>
-                    <p class="text">人生在世，孤苦无比，如果得知原有些人和你一般孤苦，是否心里会好受些？看《迷失东京》前，未曾料想会收获这样一种感觉。</p>
-                    <span class="text2">更新到第3集</span>
-                </div>
-                <span class="info2">
-                    <span>下单时间:<span class="day">9/12</span><span class="time">9:13</span></span>
-                    <span class="price">15.99</span>
-                </span>
-            </a>
-            `
-        }
-        $('.myMovieList').append(tpl).removeClass('empty')
-    }
-    // createMyMovie();
-
-
-    // 清空所有电影列表
-    function emptyMyMovie() {
-        $('.myMovieList').empty().addClass('empty');
-    }
 }
