ad61e6a007ec3e9d17e386e5541d8c1429dc09a3
Moer <199525807@qq.com>
2016-11-03 11:53:05 +0800

    用户反馈与报错接口对接完成

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,9 +1,29 @@
-    $('#feedbackSubmit').click(function () {
-        $.msg({
-            text: '小编已接到圣旨，谢谢反馈！',
-            timeout: 3000,
-            callback: () => {
-                $.router.back();
+$('#feedbackSubmit').click(function () {
+
+    $.ajax({
+        url: "http://118.178.136.60:8001/rest/user/addFeedBack",
+        data: {
+            openId: window.openId,
+            content: $('#feedbackText').val()
+        },
+        success: (res) => {
+            console.log(res);
+            if (res.STATUS == 1) {
+                $.msg({
+                    text: '小编已接到圣旨，谢谢反馈！',
+                    timeout: 3000,
+                    callback: () => {
+                        $.router.back();
+                    }
+                })
+            } else {
+                $.msg('反馈出现了问题，估计系统繁忙，请稍后试试！')
             }
-        })
-    })
+        },
+        error: (e) => {
+            console.log(e);
+            $.msg('系统繁忙，请稍后试试！')
+        }
+    });
+
+})
