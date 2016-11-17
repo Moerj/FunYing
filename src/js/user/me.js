8d693184877a5e28d4ce0ccff6bd1449207944f8
Moer <199525807@qq.com>
2016-11-17 15:36:03 +0800

    增加头像上传时的前端压缩

--- src/js/user/me.js
+++ src/js/user/me.js
@@ -109,6 +109,8 @@
 
     //头像上传
     $uploadPicker.change(function () {
+
+        // 将ftp返回的图片传给后台进行录入更新
         function _updateImg(imgUrl) {
             $.ajax({
                 url: "http://118.178.136.60:8001/rest/user/updateImg",
@@ -138,29 +140,53 @@
         let fileObj = v_this.get(0).files;
         let url = "http://118.178.136.60:8080/ercsvc-upload/upload";
         let localImgSrc = window.URL.createObjectURL($uploadPicker[0].files[0]);
-        formdata.append("imgFile", fileObj[0]);
-        $.showPreloader('正在上传头像')
-        $.ajax({
-            url: url,
-            type: 'post',
-            data: formdata,
-            cache: false,
-            contentType: false,
-            processData: false,
-            dataType: "json",
-            success: (data) => {
-                console.log('上传结果：', data);
-                if (data[0].result) {
-                    $headimg.attr('src', localImgSrc); //更新新头像
-                    _updateImg('http://wechat.94joy.com/img' + data[0].result)
+
+        $.showPreloader('正在压缩图片')
+        // 压缩
+        lrz(fileObj[0],{
+            width: 120
+        })
+        .then(function (rst) {
+            // 处理成功会执行
+            console.log('图片已压缩：',rst);
+
+            formdata.append("imgFile", rst.file);
+            $.showPreloader('正在上传头像')
+
+            // 上传到ftp
+            $.ajax({
+                url: url,
+                type: 'post',
+                data: formdata,
+                cache: false,
+                contentType: false,
+                processData: false,
+                dataType: "json",
+                success: (data) => {
+                    console.log('上传结果：', data);
+                    if (data[0].result) {
+                        $headimg.attr('src', localImgSrc); //更新新头像
+                        _updateImg('http://wechat.94joy.com/img' + data[0].result)
+                    }
+                },
+                error: () => {
+                    $.alert('上传失败，请稍后再试！')
+                    console.error('ftp部分头像上传失败')
+                    $.hidePreloader();
                 }
-            },
-            error: () => {
-                $.alert('上传失败，请稍后再试！')
-                console.error('ftp部分头像上传失败')
-                $.hidePreloader();
-            }
+            });
+            
+        })
+        .catch(function (err) {
+            // 处理失败会执行
+            $.alert('图片压缩失败，请换一张试试')
+            $.hidePreloader();
+        })
+        .always(function () {
+            // 不管是成功失败，都会执行
         });
+
+        
         return false;
     });
 
